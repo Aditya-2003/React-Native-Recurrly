e46ce1fbc3eeecd832fccbf9d080eb49a6a6b222
Aditya Shrivas <adityashrivas95@gmail.com>
2026-05-31 07:58:31 +0000

    Working App

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,36 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+import { tabs } from '../../constants/data';
+import { View, Image } from 'react-native';
+import "../../global.css"
+import clsx from 'clsx';
 
 export default function TabLayout() {
-
+ const TabIcon = ({focused, icon} : TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+           <View className={clsx("tabs-pill", focused && "tabs-active")}>
+                <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+           </View>
+      </View>
+    )
+ }
   return (
+
     <Tabs screenOptions={{
-        headerShown: false,
       }}>
+        {tabs.map((tab) => (
+          <Tabs.Screen
+            key={tab.name}
+            name={tab.name}
+            options={{ 
+              title: tab.title, 
+              tabBarIcon: ({focused}) => (
+                  <TabIcon focused={focused} icon={tabs.icon} />
+              )
+            }}
+          />
+        ))}
     </Tabs>
   );
 }
